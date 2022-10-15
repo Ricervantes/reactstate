c8939813263b5e28a0444311506452c29a2e8f24
Ricervantes <racervantes.dwfsc9@ucamp.utel.edu.mx>
2022-10-15 12:13:59 -0500

    inflar globo

---
+++ src/App.js
@@ -0,0 +1,48 @@
+
+import './App.css';
+import { useState } from 'react';
+function App() {
+  const [aireGlobo, setAireGlobo] = useState(5);
+  const [boom, setBoom] = useState(false);
+  function agregarAire() {
+    if (aireGlobo < 15) {
+      setAireGlobo(aireGlobo + 1);
+    }else{
+      setBoom(true);
+    }
+  }
+  function restarAire() {
+    if (aireGlobo > 5) {
+      setAireGlobo(aireGlobo - 1);
+    }
+    
+  if(boom){
+setBoom(false);
+setAireGlobo(5);
+  }
+
+  }
+
+  return (
+    <div className="app">
+      <div className="container text-center">
+        <div className="row align-items-center all-height">
+          <div className="col">
+            <div className="d-grid gap-2 col-6 mx-auto">
+              <button className="btn btn-success" type="button" onClick={agregarAire} disabled={boom}>+Aire</button>
+              <button className="btn btn-danger" type="button" onClick={restarAire} disabled={aireGlobo === 5}>-Aire</button>
+            </div>
+          </div>
+          <div className="col">
+
+            {boom && <h1 style={{ fontSize: `15rem` }}>💥 </h1>}
+
+           {!boom &&<h1 style={{ fontSize: `${aireGlobo}rem` }}>🎈</h1>} 
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default App;
